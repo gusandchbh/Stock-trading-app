db839d4df4f178d64282ac6695ec309f94f5b18f
christopher <gusandchbh@student.gu.se>
2022-06-23 21:17:49 +0200

    minor changes to account class and add tests

--- BankProject/src/main/java/Account.java
+++ BankProject/src/main/java/Account.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Account {
     private final String accountNumber;
     private Double balance = 0.0;
@@ -6,15 +8,54 @@ public class Account {
         this.accountNumber = accountNumber;
     }
 
+    public void transfer(Account receivingAccount, double amount) {
+        if (this.balance >= amount) {
+            this.balance -= amount;
+            receivingAccount.balance += amount;
+        }
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            this.balance += amount;
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (this.balance >= amount) {
+            this.balance -= amount;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return accountNumber.equals(account.accountNumber) && balance.equals(account.balance);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountNumber, balance);
+    }
 
     public String getAccountNumber() {
         return accountNumber;
     }
+
     public double getBalance() {
         return balance;
     }
 
     public void setBalance(Double balance) {
         this.balance = balance;
+    }
+
+    public static void main(String[] args) {
+        Account account1 = new Account("12345");
+        Account account2 = new Account("12345");
+        account1.deposit(-1000.0);
+        System.out.println(account1.getBalance());
     }
 }
--- BankProject/src/test/java/AccountTest.java
+++ BankProject/src/test/java/AccountTest.java
@@ -1,2 +1,55 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
 public class AccountTest {
+
+    Account account1 = new Account("12345");
+    Account account2 = new Account("54321");
+
+    @Test public void shouldBeEqual() {
+        assertEquals(account1, account1);
+    }
+
+    @Test public void shouldNotBeEqual() {
+        assertNotEquals(account1, account2);
+    }
+
+    @Test
+    public void shouldDeposit() {
+        account1.deposit(100);
+        assertEquals(account1.getBalance(), 100, 0);
+    }
+
+    @Test public void shouldWithdraw() {
+        account1.deposit(100);
+        account1.withdraw(50);
+        assertEquals(account1.getBalance(), 50, 0);
+    }
+
+    @Test public void shouldTransfer() {
+        account1.deposit(100);
+        account2.deposit(50);
+        account1.transfer(account2, 50);
+        assertEquals(account1.getBalance(), 50, 0);
+        assertEquals(account2.getBalance(), 100, 0);
+    }
+
+    @Test public void shouldNotTransfer(){
+        account1.deposit(100);
+        account2.deposit(50);
+        account1.transfer(account2, 101);
+        assertEquals(account1.getBalance(), 100, 0);
+    }
+
+    @Test public void shouldNotAllowNegativeBalance() {
+        account1.deposit(100);
+        account1.withdraw(101);
+        assertEquals(account1.getBalance(), 100, 0);
+    }
+    @Test public void shouldNotAllowNegativeDeposit() {
+        account1.deposit(-100);
+        assertEquals(account1.getBalance(), 0, 0);
+    }
 }
