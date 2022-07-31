c96ce304e77998b172830db421bc207450b1bba1
Ludovic Pouey <gogopex@gmail.com>
2022-07-31 19:29:01 +0200

    bonqa folder structure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,4 @@
-<<<<<<< HEAD:BankProject/src/main/java/Main.java
 import com.bonqa.bonqa.view.Menu;
-=======
-import view.Menu;
->>>>>>> 07c1e994f1a798c490b52d296aca6b89d9dc9c32:src/main/java/Main.java
 
 public class Main {
     public static void main(String[] args) {
