ac6ea731adc8f0725e2c48ab3292470d7f1ad4af
Christopher <gusandchbh@student.gu.se>
2022-12-30 17:30:04 +0100

    Add Kanye quote to login screen

--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -5,10 +5,9 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
-
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
+
---
+++ src/frontend/src/views/Login.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+  <div class="quote">
+    <h1> '{{this.quote}}' <br> - Ye</h1>
+  </div>
+  <form @submit.prevent="login">
+    <label for="username">Username:</label>
+    <input id="username" v-model="username" />
+    <br />
+    <label for="password">Password:</label>
+    <input id="password" v-model="password" type="password" />
+    <br />
+    <button type="submit">Log In</button>
+  </form>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  name: 'Login',
+  data() {
+    return {
+      username: '',
+      password: '',
+      quote: '',
+    }
+  },
+  created() {
+    this.getQuote()
+  },
+  methods: {
+    async login() {
+      try {
+        const response = await axios.get('https://api.kanye.rest/')
+        console.log(response)
+        // login successful, store user data or set cookie
+      } catch (error) {
+        console.log(error)
+      }
+    }, async getQuote() {
+      try {
+        const response = await axios.get('https://api.kanye.rest/')
+        this.quote = response.data.quote
+      } catch (error){
+        console.log(error)
+      }
+    }
+  }
+}
+
+</script>
