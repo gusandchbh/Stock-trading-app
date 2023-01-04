97a48f70251e6753023da502172b3d1dd4626b4e
Christopher <gusandchbh@student.gu.se>
2023-01-04 10:46:44 +0100

    Add ability to register new user through the frontend

--- src/frontend/src/views/Register.vue
+++ src/frontend/src/views/Register.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="app">
-    <h1>{{ test }}</h1>
-
     <div class="register-container q-pa-md" style="max-width: 400px">
       <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
         <q-input
@@ -27,22 +25,6 @@
         <q-input
           :input-style="{ height: '50px' }"
           filled
-          v-model="firstName"
-          label="Your firstname *"
-          lazy-rules="ondemand"
-          :rules="[(val) => (val && val.length > 0) || 'Please type something']"
-        />
-        <q-input
-          :input-style="{ height: '50px' }"
-          filled
-          v-model="lastName"
-          label="Your lastname *"
-          lazy-rules="ondemand"
-          :rules="[(val) => (val && val.length > 0) || 'Please type something']"
-        />
-        <q-input
-          :input-style="{ height: '50px' }"
-          filled
           v-model="email"
           label="Your email *"
           lazy-rules="ondemand"
@@ -51,37 +33,6 @@
               rules.email(val) || 'Please enter a valid email address',
           ]"
         />
-        <q-input
-          filled
-          v-model="birthDate"
-          label="Your date of birth *"
-          mask="date"
-          lazy-rules="ondemand"
-          :rules="validateDate"
-        >
-          <template v-slot:append>
-            <q-icon name="event" class="cursor-pointer">
-              <q-popup-proxy>
-                <q-date v-model="birthDate">
-                  <div class="row items-center justify-end q-gutter-sm">
-                    <q-btn
-                      label="Reset"
-                      color="primary"
-                      flat
-                      @click="resetDate"
-                      v-close-popup
-                    />
-                    <q-btn label="OK" color="primary" flat v-close-popup />
-                  </div>
-                </q-date>
-              </q-popup-proxy>
-            </q-icon>
-          </template>
-        </q-input>
-        <div>
-          <q-radio v-model="gender" val="Male" label="Male" />
-          <q-radio v-model="gender" val="Female" label="Female" />
-        </div>
         <div>
           <q-btn label="Submit" type="submit" color="primary" />
           <q-btn
@@ -98,63 +49,40 @@
 </template>
 
 <script>
-import { date, Quasar } from "quasar";
-const { addToDate } = date;
 export default {
   name: "RegisterPage",
   data() {
     return {
       username: "",
       password: "",
-      firstName: "",
-      lastName: "",
       email: "",
-      birthDate: "",
-      gender: "",
-      today: date.formatDate(Date.now(), "YYYY/MM/DD"),
-      test: "",
     };
-  },
-  computed: {
-    validateDate() {
-      return [
-        (v) => !!v || "Choose a date",
-        (v, rules) => rules.date(v) || "Please enter a valid date",
-        (v) => v.split("/")[0] < 1900 || "Choose a date after 1900",
-      ];
-    },
   },
   methods: {
     onReset() {
       this.username = null;
       this.password = null;
-      this.firstName = null;
-      this.lastName = null;
-      this.birthDate = null;
-      this.gender = null;
-    },
-    resetDate() {
-      this.birthDate = null;
-    },
-    testmethod() {
-      this.test = this.birthDate.split("/")[0];
+      this.email = null;
     },
     onSubmit() {
-      console.log(this.birthDate);
-      if (this.gender !== "Male" && this.gender !== "Female") {
-        this.$q.notify({
-          color: "red-5",
-          textColor: "white",
-          icon: "warning",
-          message: "You need to select your gender",
-        });
-      } else {
-        this.$q.notify({
-          color: "green-4",
-          textColor: "white",
-          icon: "cloud_done",
-          message: "Submitted",
-        });
+      this.$q.notify({
+        color: "green-4",
+        textColor: "white",
+        icon: "cloud_done",
+        message: "Submitted",
+      });
+      this.register(this.username, this.password, this.email);
+    },
+    async register(username, password, email) {
+      const response = await fetch("http://localhost:8080/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password, email }),
+      });
+      if (response.status === 201) {
+        console.log("CREATED!");
       }
     },
   },
@@ -176,8 +104,5 @@ export default {
   justify-content: center;
   align-content: center;
   align-items: center;
-}
-.lol {
-  size: 100px;
 }
 </style>
