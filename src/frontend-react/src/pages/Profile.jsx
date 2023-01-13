68fc44462d37e786bbd9aa8002b08629629edc71
Christopher <gusandchbh@student.gu.se>
2023-01-13 16:57:03 +0100

    Minor fix

--- src/frontend-react/src/pages/Profile.jsx
+++ src/frontend-react/src/pages/Profile.jsx
@@ -3,8 +3,6 @@ import { Card } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
 
 export const Profile = () => {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
   const { currentUser } = useAuth();
 
   return (
@@ -18,15 +16,7 @@ export const Profile = () => {
         }}
       >
         <Card.Body className="profile-card">
-          {loading ? (
-            <p>Loading...</p>
-          ) : error ? (
-            <p>Error: {error.message}</p>
-          ) : (
-            <>
               <h3>Welcome {currentUser}</h3>
-            </>
-          )}
         </Card.Body>
       </Card>
       <div className="w-100 text-center mt-2"></div>
