2f88d9b7f15f99591a55c6b0a8e628092fed2dcd
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2023-03-18 09:18:45 +0100

    removes errors

--- src/frontend-react/src/pages/Profile.jsx
+++ src/frontend-react/src/pages/Profile.jsx
@@ -1,30 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Card } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
 
 export const Profile = () => {
+  const { currentUser } = useAuth();
+  console.log(currentUser);
 
-    const { currentUser } = useAuth();
-    console.log(currentUser)
-
-    return (
-        <div>
-            <Card
-                className="text-center"
-                style={{
-                    backgroundColor: "transparent",
-                    border: "none",
-                    height: "75vh",
-                }}
-            >
-                <Card.Body className="profile-card">
-                            <h1>Welcome {!currentUser ?  "no user" : currentUser}!</h1>
-                    )}
-                </Card.Body>
-            </Card>
-            <div className="w-100 text-center mt-2"></div>
-        </div>
-    );
+  return (
+    <div>
+      <Card
+        className="text-center"
+        style={{
+          backgroundColor: "transparent",
+          border: "none",
+          height: "75vh",
+        }}
+      >
+        <Card.Body className="profile-card">
+          {/* <h1>Welcome {user}!</h1> */}
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2"></div>
+    </div>
+  );
 };
 
 export default Profile;
