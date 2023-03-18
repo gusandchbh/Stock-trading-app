a1e17c7356192afe114024b67ca31ceec5189e36
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2023-03-18 09:01:44 +0100

    fixes header

--- src/frontend-react/src/components/Header.jsx
+++ src/frontend-react/src/components/Header.jsx
@@ -1,54 +1,77 @@
 import React from "react";
-import {Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
-import {Button} from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import "../App.css";
 
 export const Header = () => {
-    const { currentUser, logout } = useAuth()
-    const navigate = useNavigate()
+  const { currentUser, logout } = useAuth();
+  const navigate = useNavigate();
 
-    const handleLogout = () => {
-        logout()
-        navigate("/login")
-    }
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
 
-    return (
+  return (
     <div
       style={{
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-evenly",
+        display: "grid",
+        gridTemplateColumns: "1fr auto",
+        alignItems: "center",
+        backgroundColor: "#91b391",
+        height: "120px",
+        padding: "0 1rem",
+        width: "100%",
       }}
     >
-        <Link to="/">
-            <h1 style={{ color: "#333" }}>Home</h1>
+      <div style={{ gridRow: "1", gridColumn: "1", textAlign: "center" }}>
+        <Link to="/" style={{ textDecoration: "none", fontFamily: "Anton" }}>
+          <h1 style={{ color: "#333", fontSize: "2.5rem" }}>Stock Tradify</h1>
         </Link>
-        {!currentUser && (
-      <Link to="/login">
-        <h1 style={{ color: "#333" }}>Login</h1>
-      </Link>
-
-        )}
-        {!currentUser && (
-            <Link to="/register">
-                <h1 style={{ color: "#333" }}>Register</h1>
-            </Link>)
-        }
-        {currentUser && (
-            <Link to="/stocks">
-                <h1 style={{ color: "#333" }}>Stocks</h1>
+      </div>
+      <div
+        style={{ display: "flex", justifyContent: "flex-end", gridColumn: "2" }}
+      >
+        {!currentUser ? (
+          <>
+            <Link
+              to="/login"
+              style={{ textDecoration: "none", marginRight: "1rem" }}
+            >
+              <h3 style={{ color: "#333" }}>Sign In</h3>
             </Link>
-
-        )}
-        {currentUser && (
-            <>
-            <Link to="/profile">
-                <h1 style={{ color: "#333" }}>Profile</h1>
+            {/* <Link to="/register" style={{ textDecoration: "none" }}>
+              <h3 style={{ color: "#333" }}>Sign Up</h3>
+            </Link> */}
+          </>
+        ) : (
+          <>
+            <Link
+              to="/stocks"
+              style={{ textDecoration: "none", marginRight: "1rem" }}
+            >
+              <h3 style={{ color: "#333" }}>Stocks</h3>
             </Link>
-                <Button style={{ color: "#333" }} onClick={handleLogout}>Sign out</Button>
-            </>
+            <Link to="/profile" style={{ textDecoration: "none" }}>
+              <h3 style={{ color: "#333" }}>Profile</h3>
+            </Link>
+            <Button
+              style={{
+                backgroundColor: "transparent",
+                border: "none",
+                color: "#333",
+                fontSize: "1.7rem",
+                fontWeight: "500",
+                marginLeft: "1rem",
+              }}
+              onClick={handleLogout}
+            >
+              Sign out
+            </Button>
+          </>
         )}
-
+      </div>
     </div>
   );
 };
