f766214939802ff64a476277f8cd5661e9e0ac33
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2023-03-18 09:15:57 +0100

    changes layout

--- src/frontend-react/src/pages/Login.jsx
+++ src/frontend-react/src/pages/Login.jsx
@@ -4,87 +4,88 @@ import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
 const Login = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState("");
-    const [loading, setLoading] = useState(false);
-    const { login } = useAuth();
-    const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const { login } = useAuth();
+  const navigate = useNavigate();
 
-    async function handleSubmit(e) {
-        e.preventDefault();
+  async function handleSubmit(e) {
+    e.preventDefault();
 
-        try {
-            setError("");
-            setLoading(true);
-            await login(username, password);
-            navigate("/");
-        } catch (error) {
-            setError(error.toString());
-            navigate("/login");
-        }
-        setLoading(false);
+    try {
+      setError("");
+      setLoading(true);
+      await login(username, password);
+      navigate("/");
+    } catch (error) {
+      setError(error.toString());
+      navigate("/login");
     }
-    return (
-        <div
-            style={{
-                display: "flex",
-                justifyContent: "center",
-                flexDirection: "column",
-                alignContent: "center",
-                alignItems: "center",
-                marginTop: "5rem",
-                marginBottom: "5rem",
-            }}
-        >
-            <Card
+    setLoading(false);
+  }
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignContent: "center",
+        alignItems: "center",
+        height: "90vh",
+      }}
+    >
+      <Card
+        className="text-center"
+        style={{
+          width: "35rem",
+          height: "30rem",
+          padding: "1rem",
+          marginTop: "-2rem",
+        }}
+      >
+        <Card.Body>
+          <h2 className="text-center mb-4">Sign In</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id="username">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
                 className="text-center"
-                style={{
-                    width: "22rem",
-                    backgroundColor: "white",
-                    borderRadius: "10%",
+                required
+                onChange={(e) => {
+                  setUsername(e.target.value);
                 }}
-            >
-                <Card.Body>
-                    <h2 className="text-center mb-4">Log In</h2>
-                    {error && <Alert variant="danger">{error}</Alert>}
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group id="username">
-                            <Form.Label>Username</Form.Label>
-                            <Form.Control
-                                className="text-center"
-                                required
-                                onChange={(e) => {
-                                    setUsername(e.target.value);
-                                }}
-                            />
-                        </Form.Group>
-                        <Form.Group id="password">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control
-                                className="text-center"
-                                type="password"
-                                required
-                                onChange={(event) => {
-                                    setPassword(event.target.value);
-                                }}
-                            />
-                        </Form.Group>
-                        <Button disabled={loading} className="w-40 mt-4" type="submit">
-                            Log In
-                        </Button>
-                    </Form>
-                    <div className="w-100 text-center mt-2">
-                        <Link to="/forgot-password">Forgot Password?</Link>
-                    </div>
-                </Card.Body>
-            </Card>
+              />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label className="mt-2">Password</Form.Label>
+              <Form.Control
+                className="text-center"
+                type="password"
+                required
+                onChange={(event) => {
+                  setPassword(event.target.value);
+                }}
+              />
+            </Form.Group>
+            <Button disabled={loading} className="w-100 mt-4" type="submit">
+              Sign in
+            </Button>
+          </Form>
+          <div style={{ display: "flex", marginTop: "1rem" }}>
             <div className="w-100 text-center mt-4">
-                Need an account? <Link to="/register">Sign Up</Link>
+              <Link to="/forgot-password">Forgot Password?</Link>
             </div>
-            <h1 style={{ color: "transparent" }}>Test</h1>
-        </div>
-    );
+            <div className="w-100 text-center mt-4">
+              Need an account? <Link to="/register">Sign Up</Link>
+            </div>
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 
 export default Login;
