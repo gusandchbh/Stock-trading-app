5ae9be562a11cad4c2996400e4210b49ccb2aa7d
Christopher <gusandchbh@student.gu.se>
2023-05-30 10:15:56 +0200

    Add tests for Password Validator

--- src/test/java/com/bonqa/bonqa/unit_tests/controller/UserControllerTest.java
+++ src/test/java/com/bonqa/bonqa/unit_tests/controller/UserControllerTest.java
@@ -1,4 +1,4 @@
-package com.bonqa.bonqa.unit_tests;
+package com.bonqa.bonqa.unit_tests.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
