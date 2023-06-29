dee1984aa0c8bf59b5a51324b761a032d8c69aa7
Christopher <gusandchbh@student.gu.se>
2023-06-29 13:44:55 +0200

    Add styling to portfolio page

--- src/frontend-react/src/components/TradeList.jsx
+++ src/frontend-react/src/components/TradeList.jsx
@@ -1,37 +1,54 @@
 import React from "react";
-import { Table } from "react-bootstrap";
+import { Table, Spinner } from "react-bootstrap";
+import moment from "moment";
 
-const PortfolioTradeList = ({ trades }) => {
-  if (!trades) {
-    return <p>Loading trades...</p>;  // or return null or some other loading spinner
-  }
+const TradeRow = ({ trade }) => (
+    <tr className="text-light">
+        <td>{trade.tradeType}</td>
+        <td>{trade.shares}</td>
+        <td>${trade.pricePerShare.toFixed(2)}</td>
+        <td>{moment(trade.createDate).format('YYYY/MM/DD')}</td>
+        <td>{trade.marketStock.name}</td>
+    </tr>
+);
 
-  return (
-      <Table striped bordered hover>
-        <thead>
-        <tr>
-          <th>Type</th>
-          <th>Shares</th>
-          <th>Price Per Share</th>
-          <th>Date</th>
-          <th>Stock</th>
-          <th>Price</th>
-        </tr>
-        </thead>
-        <tbody>
-        {trades.map((trade, index) => (
-            <tr key={index}>
-              <td>{trade.tradeType}</td>
-              <td>{trade.shares}</td>
-              <td>${trade.pricePerShare.toFixed(2)}</td>
-              <td>{new Date(trade.createDate).toLocaleString().substring(0, 10)}</td>
-              <td>{trade.marketStock.name} </td>
-              <td>${trade.marketStock.price.toFixed(2)}</td>
+const PortfolioTradeList = ({ trades, loading, error }) => {
+    if (loading) {
+        return (
+            <div className="d-flex justify-content-center text-light">
+                <Spinner animation="border" role="status">
+                    <span className="sr-only">Loading trades...</span>
+                </Spinner>
+            </div>
+        );
+    }
+
+    if (error) {
+        return <p className="text-light">Error loading trades: {error.message}</p>;
+    }
+
+    if (!trades.length) {
+        return <p className="text-light">Trades will be shown here</p>;
+    }
+
+    return (
+        <Table striped bordered hover variant="dark">
+            <thead>
+            <tr>
+                <th>Type</th>
+                <th>Shares</th>
+                <th>Price</th>
+                <th>Date</th>
+                <th>Stock</th>
             </tr>
-        ))}
-        </tbody>
-      </Table>
-  );
+            </thead>
+            <tbody>
+            {trades.map((trade) => (
+                <TradeRow key={trade.id} trade={trade} />
+            ))}
+            </tbody>
+        </Table>
+    );
 };
 
 export default PortfolioTradeList;
