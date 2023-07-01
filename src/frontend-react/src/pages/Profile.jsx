83ae6a6febd8b0ae098f298418a47fe0f41c225e
Christopher <gusandchbh@student.gu.se>
2023-07-01 11:34:35 +0200

    Fix issues related to updating password, style profile page and add deletion

--- src/frontend-react/src/pages/Profile.jsx
+++ src/frontend-react/src/pages/Profile.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
-import {Card, Form, Button, Alert, Container, Row, Col} from "react-bootstrap";
+import { Card, Form, Button, Alert, useAuth } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
-import { API } from "../api"
+import { API } from "../api";
+import { useHistory } from "react-router-dom";
+import 'simplebar-react/dist/simplebar.min.css';
 
 export const Profile = () => {
-    const { userToken } = useAuth();
+    const { userToken, logout } = useAuth();
     const [email, setEmail] = useState("");
-    const [oldPassword, setOldPassword] = useState("");
     const [newPassword, setNewPassword] = useState("");
     const [message, setMessage] = useState("");
     const [error, setError] = useState("");
+
+    const history = useHistory();
 
     const updateEmail = async (e) => {
         e.preventDefault();
@@ -27,76 +30,68 @@ export const Profile = () => {
         e.preventDefault();
         try {
             const response = await API.put(`/api/v1/users/update/password/${userToken.userId}`, {
-                oldPassword: oldPassword,
-                newPassword: newPassword
+                password: newPassword
             });
 
             setMessage(response.data);
         } catch (error) {
-            setError(error.message);
+            setError(error.response.data);
+        }
+    }
+
+    const deleteUser = async () => {
+        if(window.confirm("Are you sure you want to delete your account? This action is irreversible.")) {
+            try {
+                const response = await API.delete(`/api/v1/users/delete/${userToken.userId}`);
+                setMessage(response.data);
+                logout();
+                history.push('/login');
+            } catch (error) {
+                setError(error.response.data);
+            }
         }
     }
 
     return (
-        <div style={{ height: "100vh" }}>
-            <Container>
-                <Row>
-                    <Col xs={12} md={6} className="mx-auto">
-                        <Card
-                            className="text-center"
-                            style={{
-                                backgroundColor: "transparent",
-                                border: "none",
-                                height: "75vh",
-                            }}
-                        >
-                            <Card.Body className="profile-card" style={{maxWidth: "500px", margin: "0 auto"}}>
-                                <h1>Welcome {userToken ? userToken.sub : "no user"}!</h1>
-                                {message && <Alert variant="success">{message}</Alert>}
-                                {error && <Alert variant="danger">{error}</Alert>}
-                                <Form onSubmit={updateEmail} style={{maxWidth: "500px", margin: "0 auto"}}>
-                                    <Form.Group>
-                                        <Form.Label>Email address</Form.Label>
-                                        <Form.Control
-                                            type="email"
-                                            required
-                                            value={email}
-                                            onChange={e => setEmail(e.target.value)}
-                                        />
-                                    </Form.Group>
-                                    <Button style={{marginTop: "1rem"}} variant="primary" type="submit">
-                                        Update Email
-                                    </Button>
-                                </Form>
-                                <Form onSubmit={updatePassword} className="mt-4" style={{maxWidth: "500px", margin: "0 auto"}}>
-                                    <Form.Group>
-                                        <Form.Label>Old Password</Form.Label>
-                                        <Form.Control
-                                            type="password"
-                                            required
-                                            value={oldPassword}
-                                            onChange={e => setOldPassword(e.target.value)}
-                                        />
-                                    </Form.Group>
-                                    <Form.Group>
-                                        <Form.Label>New Password</Form.Label>
-                                        <Form.Control
-                                            type="password"
-                                            required
-                                            value={newPassword}
-                                            onChange={e => setNewPassword(e.target.value)}
-                                        />
-                                    </Form.Group>
-                                    <Button style={{marginTop: "1rem"}} variant="primary" type="submit">
-                                        Change Password
-                                    </Button>
-                                </Form>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
-            <div className="w-100 text-center mt-2"></div>
+        <div className="vh-100 bg-dark d-flex text-light">
+            <Card className="text-center bg-dark border-0 mx-auto" style={{ width: "90vw", height: "80vh" }}>
+                <Card.Body className="d-flex flex-column align-items-center text-light">
+                    <h1>Welcome {userToken ? userToken.sub : "no user"}!</h1>
+                    {message && <Alert variant="success">{message}</Alert>}
+                    {error && <Alert variant="danger">{error}</Alert>}
+                    <Form onSubmit={updateEmail} style={{ maxWidth: "500px", margin: "0 auto" }}>
+                        <Form.Group>
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control
+                                type="email"
+                                required
+                                value={email}
+                                onChange={e => setEmail(e.target.value)}
+                            />
+                        </Form.Group>
+                        <Button style={{ marginTop: "1rem" }} variant="primary" type="submit">
+                            Update Email
+                        </Button>
+                    </Form>
+                    <Form onSubmit={updatePassword} className="mt-4" style={{ maxWidth: "500px", margin: "0 auto" }}>
+                        <Form.Group>
+                            <Form.Label>New Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                required
+                                value={newPassword}
+                                onChange={e => setNewPassword(e.target.value)}
+                            />
+                        </Form.Group>
+                        <Button style={{ marginTop: "1rem" }} variant="primary" type="submit">
+                            Change Password
+                        </Button>
+                    </Form>
+                    <Button variant="danger" onClick={deleteUser} className="mt-4">
+                        Delete Account
+                    </Button>
+                </Card.Body>
+            </Card>
         </div>
     );
 };
