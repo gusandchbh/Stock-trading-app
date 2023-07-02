a4d06b6c841c299d8f4d54608d05cc587fb05dee
Christopher <gusandchbh@student.gu.se>
2023-07-02 15:38:38 +0200

    Add ability to search account statement

--- src/frontend-react/src/components/TradeList.jsx
+++ src/frontend-react/src/components/TradeList.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Table, Spinner } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Table, Spinner, Form } from "react-bootstrap";
 import moment from "moment";
 
 const TradeRow = ({ trade }) => (
@@ -13,6 +13,22 @@ const TradeRow = ({ trade }) => (
 );
 
 const PortfolioTradeList = ({ trades, loading, error }) => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [filteredTrades, setFilteredTrades] = useState(trades);
+
+    useEffect(() => {
+        const lowercasedSearchTerm = searchTerm.toLowerCase();
+        const newFilteredTrades = trades.filter((trade) =>
+            trade.tradeType.toLowerCase().includes(lowercasedSearchTerm) ||
+            trade.shares.toString().includes(lowercasedSearchTerm) ||
+            trade.pricePerShare.toFixed(2).includes(lowercasedSearchTerm) ||
+            moment(trade.createDate).format('YYYY/MM/DD').includes(lowercasedSearchTerm) ||
+            trade.marketStock.name.toLowerCase().includes(lowercasedSearchTerm)
+        );
+
+        setFilteredTrades(newFilteredTrades);
+    }, [searchTerm, trades]);
+
     if (loading) {
         return (
             <div className="d-flex justify-content-center text-light">
@@ -31,23 +47,47 @@ const PortfolioTradeList = ({ trades, loading, error }) => {
         return <p className="text-light">Trades will be shown here</p>;
     }
 
+    if (!filteredTrades.length) {
+        return (
+            <div>
+                <Form.Control
+                    type="text"
+                    placeholder="Search..."
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    className="my-3"
+                />
+                <p className="text-light">No trades found</p>
+            </div>
+        );
+    }
+
     return (
-        <Table striped bordered hover variant="dark">
-            <thead>
-            <tr>
-                <th>Type</th>
-                <th>Shares</th>
-                <th>Price</th>
-                <th>Date</th>
-                <th>Stock</th>
-            </tr>
-            </thead>
-            <tbody>
-            {trades.map((trade) => (
-                <TradeRow key={trade.id} trade={trade} />
-            ))}
-            </tbody>
-        </Table>
+        <div>
+            <Form.Control
+                type="text"
+                placeholder="Search..."
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                className="my-3"
+            />
+            <Table striped bordered hover variant="dark">
+                <thead>
+                <tr>
+                    <th>Type</th>
+                    <th>Shares</th>
+                    <th>Price</th>
+                    <th>Date</th>
+                    <th>Stock</th>
+                </tr>
+                </thead>
+                <tbody>
+                {filteredTrades.map((trade) => (
+                    <TradeRow key={trade.id} trade={trade} />
+                ))}
+                </tbody>
+            </Table>
+        </div>
     );
 };
 
